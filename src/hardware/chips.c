6d405b6b641d53f05573bbb57bbbdb25db31eee7
titan_exasaur <tinytachyon14341@gmail.com>
2026-08-08 01:32:44 +0530

    basic chips complete
    - implemented elementary chips
    
    - implemented 16-bit variant chips
    
    - implemented multiway variant chips

--- src/hardware/chips.c
+++ src/hardware/chips.c
@@ -18,7 +18,7 @@ bool not(bool in){
 bool and(bool a, bool b){
 	bool nand_a_b = nand(a,b);
 	bool out = nand(nand_a_b,nand_a_b);
-	return out;
+	return out;	
 }
 
 bool or(bool a, bool b){
@@ -116,79 +116,106 @@ void mux4way16(
     bool ab[16];
     bool cd[16];
 
-    mux16(a, b, sel[1], ab);
-    mux16(c, d, sel[1], cd);
+    mux16(a,b,sel[1],ab);
+    mux16(c,d,sel[1],cd);
 
-    mux16(ab, cd, sel[0],out);
+    mux16(ab,cd,sel[0],out);
+}
+
+void mux8way16(
+    bool a[16],
+    bool b[16],
+    bool c[16],
+    bool d[16],
+    bool e[16],
+    bool f[16],
+    bool g[16],
+    bool h[16],
+    bool sel[3],
+    bool out[16]
+) {
+    bool abcd[16];
+    bool efgh[16];
+    bool subset[2] = {sel[1], sel[2]};
+
+    mux4way16(a,b,c,d,subset,abcd);
+    mux4way16(e,f,g,h,subset,efgh);
+
+    mux16(abcd,efgh,sel[0],out);
+}
+
+void dmux4way(
+	bool in, 
+	bool *a, 
+	bool *b, 
+	bool *c, 
+	bool *d, 
+	bool sel[2]
+){
+	bool ab;
+	bool cd;
+
+	dmux(in,sel[0],&ab,&cd);
+	dmux(ab,sel[1],a,b);
+	dmux(cd,sel[1],c,d);
+}
+
+void dmux8way(
+	bool in, 
+	bool *a, 
+	bool *b, 
+	bool *c, 
+	bool *d, 
+	bool *e, 
+	bool *f, 
+	bool *g, 
+	bool *h, 
+	bool sel[3]
+){
+	bool abcd;
+	bool efgh;
+	bool subset[2] = {sel[1], sel[2]};
+
+	dmux(in, sel[0], &abcd, &efgh);
+	dmux4way(abcd,a,b,c,d,subset);
+	dmux4way(efgh,e,f,g,h,subset);
 }
 
 // --------------------------- Chips Testing ---------------------------
 
 int main() {
-    bool inputs[4][16] = {
-        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0},
-        {0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1}
-    };
-
     int passed = 0;
     int total = 0;
-
-    for (int a = 0; a < 4; a++) {
-        for (int b = 0; b < 4; b++) {
-            for (int c = 0; c < 4; c++) {
-                for (int d = 0; d < 4; d++) {
-
-                    for (int sel = 0; sel < 4; sel++) {
-
-                        bool sel_bits[2] = {
-                            (sel >> 1) & 1,
-                            sel & 1
-                        };
-
-                        bool out[16];
-
-                        mux4way16(
-                            inputs[a],
-                            inputs[b],
-                            inputs[c],
-                            inputs[d],
-                            sel_bits,
-                            out
-                        );
-
-                        int selected;
-
-                        if (sel_bits[0] == 0 && sel_bits[1] == 0)
-                            selected = 0;
-                        else if (sel_bits[0] == 0 && sel_bits[1] == 1)
-                            selected = 1;
-                        else if (sel_bits[0] == 1 && sel_bits[1] == 0)
-                            selected = 2;
-                        else
-                            selected = 3;
-
-                        bool correct = true;
-
-                        for (int i = 0; i < 16; i++) {
-                            if (out[i] != inputs[selected][i]) {
-                                correct = false;
-                                break;
-                            }
-                        }
-
-                        total++;
-
-                        if (correct)
-                            passed++;
-                    }
-                }
-            }
+    for (int in = 0; in < 2; in++) {
+        for (int sel = 0; sel < 8; sel++) {
+            bool sel_bits[3] = {
+                (sel >> 2) & 1,
+                (sel >> 1) & 1,
+                sel & 1
+            };
+            bool a, b, c, d, e, f, g, h;
+            dmux8way(
+                in,
+                &a, &b, &c, &d, &e, &f, &g, &h,
+                sel_bits
+            );
+            bool expected[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+            if (in)
+                expected[sel] = true;
+            bool correct =
+                a == expected[0] &&
+                b == expected[1] &&
+                c == expected[2] &&
+                d == expected[3] &&
+                e == expected[4] &&
+                f == expected[5] &&
+                g == expected[6] &&
+                h == expected[7];
+            total++;
+            if (correct)
+                passed++;
         }
     }
-
     printf("Tests passed: %d/%d\n", passed, total);
-
     return 0;
 }
